324972dcfc4edbcda5e00a9ad86a28ae121a7b35
reecexlm <reecexlm@gmail.com>
2022-06-07 20:55:35 -0700

    test

--- docs/deployment-examples/aws-eks-k8-helm/terraform/route53.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/route53.tf
@@ -28,7 +28,8 @@ resource "aws_route53_record" "sep" {
   name    = "www.${data.aws_route53_zone.anchor-zone.name}"
   type    = "CNAME"
   ttl     = "300"
-  records = [data.kubernetes_ingress.sep_ingress.status.0.load_balancer.0.ingress.0.hostname]
+  #records = [data.kubernetes_ingress.sep_ingress.status.0.load_balancer.0.ingress.0.hostname]
+  records = ["abc.com"]
   depends_on = [
     helm_release.sep]
 }
