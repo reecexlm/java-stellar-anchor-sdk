1f25a885bc373cfb51f386317f7468d0cffb5892
reecexlm <reecexlm@gmail.com>
2022-06-07 20:44:16 -0700

    teapo

--- docs/deployment-examples/aws-eks-k8-helm/terraform/anchor-platform-sep-server-release.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/anchor-platform-sep-server-release.tf
@@ -35,6 +35,6 @@ resource "helm_release" "sep" {
 
     values = [templatefile("${path.module}/anchor-platform-sep-server-values.yaml",
     local.sep_template_vars)]
-    depends_on = [resource.helm_release.ingress-nginx, resource.helm_relase.reference]
+    depends_on = [resource.helm_release.ingress-nginx, resource.helm_release.reference]
 }
 
