149e3865ab59ab5123d1fb710407055404ccfc96
reecexlm <reecexlm@gmail.com>
2022-06-09 22:57:37 -0700

    fingers crossed!

--- docs/deployment-examples/aws-eks-k8-helm/terraform/anchor-platform-sep-server-release.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/anchor-platform-sep-server-release.tf
@@ -6,6 +6,16 @@ data "kubernetes_ingress" "sep" {
   depends_on = [resource.helm_release.sep] 
 }
 
+data "kubernetes_service" "ingress_reference" {
+    metadata {
+      name = "reference-server-service"
+      namespace = "anchor-platform"
+    }
+}
+
+output "k8s_service_reference" {
+  value = data.kubernetes_service.ingress_reference.status.0.load_balancer.0.ingress.0.hostname
+}
 #data "kubernetes_ingress" "reference" {
 #  metadata {
 #    name = "reference-server-ingress"
