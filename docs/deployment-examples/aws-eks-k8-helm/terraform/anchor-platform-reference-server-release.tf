f22929c55cb77a1da1b12d6c4905372c4445ae85
reecexlm <reecexlm@gmail.com>
2022-06-11 13:04:32 -0700

    s

--- docs/deployment-examples/aws-eks-k8-helm/terraform/anchor-platform-reference-server-release.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/anchor-platform-reference-server-release.tf
@@ -4,6 +4,7 @@ locals {
     #bootstrap_broker = "${element(split(",", data.aws_msk_cluster.anchor_msk.bootstrap_brokers), 0)}"
   }
 
+
   helm_chart_values = templatefile(
       "${path.module}/anchor-platform-reference-server-values.yaml",
       local.template_vars
@@ -22,7 +23,8 @@ resource "helm_release" "reference" {
   max_history      = 3
   timeout          = 600
 
-values = [local.helm_chart_values, module.eks.cluster.cluster_id]
+values = [local.helm_chart_values]
+depends_on = [module.eks.cluster_id]
 
 }
 
