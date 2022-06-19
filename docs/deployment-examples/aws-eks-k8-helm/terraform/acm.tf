601dff3decbabd83ff153f3eb9555ed58e4dd1b2
reecexlm <reecexlm@gmail.com>
2022-06-18 20:13:27 -0700

    acm1

--- docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate" "sep" {
+resource "aws_acm_certificate" "cert" {
   domain_name               = "www.${data.aws_route53_zone.anchor_zone.name}"
   validation_method         = "DNS"
 lifecycle {
@@ -6,7 +6,15 @@ lifecycle {
   }
 }
 
-resource "aws_acm_certificate_validation" "acm_certificate_validation" {
- certificate_arn = aws_acm_certificate.sep.arn
- validation_record_fqdns = [for record in aws_route53_record.sep : record.fqdn]
+resource "aws_route53_record" "cert_validation" {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.anchor_zone.zone_id}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
