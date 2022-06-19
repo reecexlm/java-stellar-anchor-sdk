020a66d7015e9354f28e9dac6701a376d5c1d790
reecexlm <reecexlm@gmail.com>
2022-06-18 19:55:30 -0700

    3.08

---
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
@@ -0,0 +1,12 @@
+resource "aws_acm_certificate" "sep" {
+  domain_name               = "www.${data.aws_route53_zone.anchor-zone.name}"
+  validation_method         = "DNS"
+lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "acm_certificate_validation" {
+ certificate_arn = aws_acm_certificate.sep.arn
+ validation_record_fqdns = [for record in aws_route53_record.sep : record.fqdn]
+}
