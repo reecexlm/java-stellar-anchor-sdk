fa8ebf9ae4806bb1c605c4f7343cb3f50e1277d9
reecexlm <reecexlm@gmail.com>
2022-06-18 22:31:20 -0700

    certifivate

--- docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
@@ -8,7 +8,7 @@ lifecycle {
 
 resource "aws_route53_record" "validation" {
   for_each = {
-    for dvo in aws_acm_certificate.sep.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
