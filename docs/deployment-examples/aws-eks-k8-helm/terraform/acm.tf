41ba2f2500bf23d4644d222148d88409cb4809ce
reecexlm <reecexlm@gmail.com>
2022-06-18 20:50:45 -0700

    acm3

--- docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
@@ -6,13 +6,6 @@ lifecycle {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.anchor_zone.zone_id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
-}
 
 resource "aws_acm_certificate_validation" "acm_certificate_validation" {
  certificate_arn = aws_acm_certificate.cert.arn
