ac3cd03217ca18d47b492f32ae1d96ab38b305fd
reecexlm <reecexlm@gmail.com>
2022-06-19 12:29:46 -0700

    eks back to single node pool

--- docs/deployment-examples/aws-eks-k8-helm/terraform/eks-cluster.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/eks-cluster.tf
@@ -330,30 +330,16 @@ module "eks" {
       
       capacity_type = "SPOT"
       instance_types = ["t3.micro"]
-      desired_size = 4
-      min_size     = 4
-      max_size     = 4
+      desired_size = 6
+      min_size     = 6
+      max_size     = 6
 
         labels = {
           Environment = "dev"
           Terraform   = "true"
-          app = "sep"
         }
     }
 
-   ref = {
-      
-      capacity_type = "SPOT"
-      instance_types = ["t3.micro"]
-      desired_size = 2
-      min_size     = 2
-      max_size     = 2
-
-        labels = {
-          app = "reference"
-          Terraform   = "true"
-        }
-    }
 
   }
 }
