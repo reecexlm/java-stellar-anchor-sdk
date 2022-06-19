5a511dce3aed4f21b60a8f2a7343e3ccf4f2c687
reecexlm <reecexlm@gmail.com>
2022-06-19 07:12:14 -0700

    acm module

--- docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
+++ docs/deployment-examples/aws-eks-k8-helm/terraform/acm.tf
@@ -1,32 +1,17 @@
-resource "aws_acm_certificate" "cert" {
-  domain_name               = "www.${data.aws_route53_zone.anchor_zone.name}"
-  validation_method         = "EMAIL"
-lifecycle {
-    create_before_destroy = true
+module "acm" {
+    source  = "terraform-aws-modules/acm/aws"
+    version = "~> 3.0"
+
+  domain_name = data.aws_route53_zone.anchor_zone.name
+  zone_id     = data.aws_route53_zone.anchor_zone.zone_id
+
+  subject_alternative_names = [
+    "*.${local.domain_name}"
+  ]
+
+  wait_for_validation = true
+
+  tags = {
+    Name = data.aws_route53_zone.anchor_zone.name
   }
-}
-
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.anchor_zone.zone_id
-}
-
-resource "aws_acm_certificate_validation" "acm_certificate_validation" {
- certificate_arn = aws_acm_certificate.cert.arn
- validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-}
-
-output "validation_records" {
-  value = aws_acm_certificate_validation.acm_certificate_validation.validation_record_fqdns
 }
