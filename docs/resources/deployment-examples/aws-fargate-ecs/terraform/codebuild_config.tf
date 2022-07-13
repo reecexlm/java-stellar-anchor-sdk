e9a33ff9ea3dab76cbe03289de6512a607b04aea
reecexlm <reecexlm@gmail.com>
2022-07-12 23:10:53 -0700

    last try for the night

--- docs/resources/deployment-examples/aws-fargate-ecs/terraform/codebuild_config.tf
+++ docs/resources/deployment-examples/aws-fargate-ecs/terraform/codebuild_config.tf
@@ -49,10 +49,13 @@ resource "aws_iam_role_policy" "codebuild_policy" {
                 ],
                 "Action": [
                     "ec2:DescribeSubnets",
-                    "ec2:DescribeSecurityGroups",
-                    "ec2:DescribeVpcs",
+                    "ec2:CreateNetworkInterface",
+                    "ec2:DescribeDhcpOptions",
                     "ec2:DescribeNetworkInterfaces",
-                    "ec2:DeleteNetworkInterface"
+                    "ec2:DeleteNetworkInterface",
+                    "ec2:DescribeSubnets",
+                    "ec2:DescribeSecurityGroups",
+                    "ec2:DescribeVpcs"
                 ]
             },
             {
